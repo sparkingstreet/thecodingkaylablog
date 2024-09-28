179975188a8efab875dbda8e4a945bf923c17540
sparkingstreet <kaydavis9413@gmail.com>
2024-09-27 22:50:32 -0400

    hamburger menu

---
+++ navigation.js
@@ -0,0 +1,10 @@
+<script>
+    function toggleMenu() {
+        var menu = document.getElementById("menu-items");
+        if (menu.style.display === "flex") {
+            menu.style.display = "none";
+        } else {
+            menu.style.display = "flex";
+        }
+    }
+</script>
